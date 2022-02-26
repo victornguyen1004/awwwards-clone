37f709e75217c6acfeefd040e7a90385b9bef360
victornguyen1004 <91527460+victornguyen1004@users.noreply.github.com>
2022-02-26 11:15:45 +0700

    Having 1 horizontal bug

--- Navbar/scripts.js
+++ Navbar/scripts.js
@@ -6,29 +6,40 @@ const loginForm = document.getElementById('login-form');
 const loginBtn = document.getElementById('login-btn');
 const loginCloseBtn = document.getElementById('login-close-btn');
 
-menuBtn.addEventListener('click', function() {
+function showDarkCover() {
     darkCover.classList.remove('hidden');
+}
+
+function hideDarkCover() {
+    darkCover.classList.add('hidden');
+}
+
+// When click menuBtn it show the darkCover and show Menu
+menuBtn.addEventListener('click', function() {
+    showDarkCover()
     sidebar.classList.remove('-translate-x-full');
 })
 
 closeBtn.addEventListener('click', function(){
-    darkCover.classList.add('hidden');
+    hideDarkCover()
     sidebar.classList.add('-translate-x-full');
 })
 
 loginBtn.addEventListener('click', function() {
-    darkCover.classList.remove('hidden');
+    showDarkCover();
     loginForm.classList.remove('hidden');
 })
 
 loginCloseBtn.addEventListener('click', function() {
-    darkCover.classList.add('hidden');
+    hideDarkCover();
     loginForm.classList.add('hidden');
 })
 
 darkCover.addEventListener('click', function() {
-    if (sidebar.classList.contains('-translate-x-full') == true) {
-        sidebar.classList.remove('-translate-x-full');
+    if (!sidebar.classList.contains('-translate-x-full')) {
+        sidebar.classList.add('-translate-x-full');
+        hideDarkCover();
     }
 })
+
 
